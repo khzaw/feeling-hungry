b091914654c9cca9530875de90beb0f12ea8a66c
Kaung Htet <kayhzaw@gmail.com>
2023-04-13 18:57:05 +0800

    Flatten of product variations across all menu categories

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -10,96 +10,90 @@ reloadOnUpdate("pages/background");
 reloadOnUpdate("pages/content/style.scss");
 
 
-const vendorURL = '/api/v5/vendors/';
-
-
-// filter out small-ish stuffs like drinks, appetizers., etc
-const filterOutNonCategories = menus => {
-  const fOut = ['beverages', 'drinks', 'appetiziers', 'appetizer']
-  return menus[0].menu_categories.filter(x => !fOut.includes(x.name.toLowerCase()));
-}
-
-const flattenProductVariations = products => {
-  return _.flatten(
-    products.map(p => p.product_variations.map(v => {
-      return { ...p, product_variations: v }
-    }))
-  )
-}
-
 const knapsack = (items, total) => {
-  // memoization baby
-  const cache = new Map();
+	// memoization baby
+	const cache = new Map();
 
-  const knapsackHelper = (n, w) => {
-    if (n === 0 || w === 0) {
-      return 0;
-    }
+	const knapsackHelper = (n, w) => {
+		if (n === 0 || w === 0) {
+			return 0;
+		}
 
-    const key = `${n}-${w}`;
-    if (cache.has(key)) {
-      return cache.get(key);
-    }
+		const key = `${n}-${w}`;
+		if (cache.has(key)) {
+			return cache.get(key);
+		}
 
-    const price = items[n - 1].product_variations.price;
-    if (price > w) {
-      const result = knapsackHelper(n - 1, w);
-      cache.set(key, result);
-      return result;
-    }
+		const price = items[n - 1].product_variations.price;
+		if (price > w) {
+			const result = knapsackHelper(n - 1, w);
+			cache.set(key, result);
+			return result;
+		}
 
-    const result = Math.max(
-      knapsackHelper(n - 1, w),
-      knapsackHelper(n - 1, w - price) + 1
-    );
-    cache.set(key, result);
-    return result;
-  }
+		const result = Math.max(
+			knapsackHelper(n - 1, w),
+			knapsackHelper(n - 1, w - price) + 1
+		);
+		cache.set(key, result);
+		return result;
+	}
 
-  const maxTotalValue = knapsackHelper(items.length, total);
+	// const maxTotalValue = knapsackHelper(items.length, total);
 
-  const chosen = [];
-  let remainingTotal = total;
-  for (let i = items.length; i > 0 && remainingTotal > 0; i--) {
-    const price = items[i - 1].product_variations.price;
-    if (knapsackHelper(i, remainingTotal) !== knapsackHelper(i - 1, remainingTotal)) {
-      chosen.push(items[i - 1]);
-      remainingTotal -= price;
-    }
-  }
+	const chosen = [];
+	let remainingTotal = total;
+	for (let i = items.length; i > 0 && remainingTotal > 0; i--) {
+		const price = items[i - 1].product_variations.price;
+		if (knapsackHelper(i, remainingTotal) !== knapsackHelper(i - 1, remainingTotal)) {
+			chosen.push(items[i - 1]);
+			remainingTotal -= price;
+		}
+	}
 
-  return chosen;
+	return chosen;
 }
 
+
+const pickADamnThing = (vendorMenus, total: number) => {
+	const FOUT = ['beverage', 'beverages', 'drinks', 'appetiziers', 'side order']
+	const menus = vendorMenus.menu_categories.filter(x => !FOUT.includes(x.name.toLowerCase()));
+	const flattenMenu = _.flatMap(menus, _.property('products'))
+	const flattenVariations = _.flatten(
+		flattenMenu.map(p => p.product_variations.map(v => ({ ...p, product_variations: v }))
+		)
+	);
+
+	console.log(flattenVariations);
+	return knapsack(flattenVariations, total)
+}
 
 let VENDOR_LOADED = false;
-
 chrome.webRequest.onCompleted.addListener(async details => {
-  if (details.url.includes('include') && !VENDOR_LOADED) {
-    VENDOR_LOADED = true;
-    try {
-      const res = await fetch(details.url);
-      const json = await res.json();
-      const vendor = json.data;
+	// needs a better check
+	if (details.url.includes('include') && !VENDOR_LOADED) {
+		VENDOR_LOADED = true;
+		try {
+			const res = await fetch(details.url);
+			const json = await res.json();
+			const vendor = json.data;
 
-      const filteredMenus = filterOutNonCategories(vendor.menus);
-      const chosen = knapsack(flattenProductVariations(filteredMenus[0].products), 20);
+			const chosen = pickADamnThing(vendor.menus[0], 30);
+			console.log('CHOSEN', chosen, chosen.map(x => x.product_variations.price));
 
-      console.log('CHOSEN', chosen, chosen.map(x => x.product_variations.price));
-
-    } catch(e) {
-      console.log(e);
-    }
-  }
+		} catch (e) {
+			console.log(e);
+		}
+	}
 }, { urls: ['*://*.fd-api.com/api/v5/vendors/*'] }, ['extraHeaders', 'responseHeaders']);
 
 
 chrome.webRequest.onBeforeRequest.addListener(details => {
-  console.log("onBeforeRequest", details.url, details);
-  const postedString = decodeURIComponent(String.fromCharCode.apply(null, new Uint8Array(details.requestBody.raw[0].bytes)));
-  console.log("postedString", postedString);
+	console.log("onBeforeRequest", details.url, details);
+	const postedString = decodeURIComponent(String.fromCharCode.apply(null, new Uint8Array(details.requestBody.raw[0].bytes)));
+	console.log("postedString", postedString);
 }, {
-  urls: ['*://*.fd-api.com/api/v5/rs/cart/calculate*'],
+	urls: ['*://*.fd-api.com/api/v5/rs/cart/calculate*'],
 }, ['requestBody', 'extraHeaders']);
 
 
