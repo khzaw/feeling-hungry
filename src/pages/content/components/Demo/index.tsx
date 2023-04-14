79a3d6448e0094d42c92c9fac1f2b8a40ce0f441
Kaung Htet <kayhzaw@gmail.com>
2023-04-14 13:11:48 +0800

    Fix

--- src/pages/content/components/Demo/index.tsx
+++ src/pages/content/components/Demo/index.tsx
@@ -17,7 +17,8 @@ refreshOnUpdate("pages/content");
 const root = document.createElement("div");
 root.id = "chrome-extension-boilerplate-react-vite-content-view-root";
 document.body.append(root);
-var buttonDiv
+
+const buttonDiv = document.createElement('div');
 
 if (window.location.href === 'https://www-st.foodpanda.sg/') {
 	const formButtonContainer = document.getElementsByClassName('button-container')[0]
@@ -35,7 +36,6 @@ if (window.location.href === 'https://www-st.foodpanda.sg/') {
 		formButtonContainer.appendChild(orSpan);
 	}
 
-	buttonDiv = document.createElement('div');
 	buttonDiv.classList.add('surprise-me-button');
 	if (formButtonContainer) {
 		formButtonContainer.appendChild(buttonDiv);
